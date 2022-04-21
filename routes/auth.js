c7a2ce10b5e14180d45b050536edab0f7f6c0e40
anishmhrzn47 <anishamhrzn47@gmail.com>
2022-04-21 16:26:56 +1000

    Authentication Complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,43 @@
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const _ = require('lodash');
-const {User} = require('../models/user');
-const mongoose = require('mongoose');
-const express = require('express');
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+//1. we export User and validate function from the mdoels/user folder
+const { User } = require("../models/user");
+
+//2. We also need mongoose, express and router
+const mongoose = require("mongoose");
+const express = require("express");
+const string = require("joi/lib/types/string");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+//3. These are all the routes.
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+  let user = await User.findOne({ email: req.body.username });
+  if (!user) {
+    let userCheck = await User.findOne({ phone: req.body.username });
+    console.log(userCheck);
+
+    if (!userCheck) { return res.status(400).send("Invalid username or password"); } else {
+      user = userCheck;
+    }
+  }
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
-
-  const token = user.generateAuthToken();
-  res.send(token);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+  res.send(_.pick(user, ["_id", "name", "email", "phone"]));
 });
 
 function validate(req) {
   const schema = {
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    username: Joi.string().min(5).max(50).required(),
+    password: Joi.string().min(5).max(255).required(),
   };
-
   return Joi.validate(req, schema);
 }
 
-module.exports = router; 
+// 4. Finally we need to export this router
+
+module.exports = router;
